696d710a7b61183f2b584752f936c4ad070a8049
yurim <leeyou6757@sasa.hs.kr>
2019-11-19 11:14:40 +0900

    git

--- yurim/game_main.py
+++ yurim/game_main.py
@@ -1,65 +1,54 @@
 import pygame
 
 
+class background:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("background.jpg")
+
+
 def move_background():
-    global bg1_x, bg2_x, bg1_y, bg2_y, bg_length, bg1, bg2, screen
+    global bg_length, bg1, bg2, screen
 
-    bg1_x -= 2
-    bg2_x -= 2
+    bg1.y -= 2
+    bg2.y -= 2
 
-    if bg1_x == -bg_length:
-        bg1_x = bg_length
+    if bg1.y == -bg_length:
+        bg1.y = bg_length
+    if bg2.y == -bg_length:
+        bg2.y = bg_length
 
-    if bg2_x == -bg_length:
-        bg2_x = bg_length
-
-    screen.blit(bg1, (bg1_x, 0))
-    screen.blit(bg2, (bg2_x, 0))
-
+    screen.blit(bg1.image, (0, bg1.y))
+    screen.blit(bg2.image, (0, bg2.y))
     pygame.display.update()
 
 
 def runGame():
-    global bg1, bg2, bg1_x, bg2_x, bg1_y, bg2_y, bg_length, clock
+    global bg1, bg2, bg_length, clock
 
     # Set background
     bg_length = 600
-    bg1_x = 0
-    bg2_x = bg_length
-    bg1_y = 0
-    bg2_y = bg_length
+    bg1 = background(0, 0)
+    bg2 = background(bg_length, bg_length)
 
     # Event loop
     crashed = False
     while not crashed:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.K_SPACE:
                 crashed = True
 
-            if event.type == pygame.KEYDOWN:    # 키보드를 누른 후 뗄 때
-                if event.key == pygame.K_UP:
-                    pass
+        move_background()
 
-                elif event.key == pygame.K_DOWN:
-                    pass
-
-            if event.type == pygame.KEYUP:  # 키보드를 누를 때
-                while event.key == pygame.K_UP:
-                    move_background()
-
-        # screen.blit(background, (0, 0))
-        # pygame.display.flip()
 
 
 def initGame():
-    global screen, airplane, clock, bg1, bg2
+    global screen, airplane, clock
 
     screen = pygame.display.set_mode((600, 600))
     pygame.display.set_caption('Plane_Crash_Game.jpg')
     airplane = pygame.image.load("airplane.jpg")
-    bg1 = pygame.image.load("background.jpg")
-    bg2 = bg1.copy()
-
     clock = pygame.time.Clock()
     runGame()
 
