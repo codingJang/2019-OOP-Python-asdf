20de118627f1791a6b1c66f8caccb05ca686d14b
yurim <leeyou6757@sasa.hs.kr>
2019-11-29 21:02:18 +0900

    score + level + addmissile

--- main_practice.py
+++ main_practice.py
@@ -14,6 +14,7 @@ from junho.airplane import *  # 장준호가 만든 Airplane 클래스
 from yurim.background import *  # 이유림이 만든 Background 클래스
 from yejun.blit_methods import *
 from yurim.button import *
+from yurim.addmissile import *
 
 
 pygame.init()
@@ -28,11 +29,13 @@ backgrounds.add(Background(0, 0), Background(bg_length, 0), Background(0, bg_len
 
 options = pygame.sprite.Group()
 
-startButton = Button((0, 223, 0), 200, 500, 400, 100, 'Choose your airplane!')
+# 시작 화면
+startButton = Button((0, 223, 0), 220, 550, 360, 80, 'Choose your airplane!')
 options.add(startButton)
 make_button(screen, options, "yurim/startBackground.png")
 options.remove(startButton)
 
+# 비행기 옵션 선택
 option1 = Button((0, 255, 0), 80, 500, 200, 100, 'Option 1')
 option2 = Button((0, 255, 0), 300, 500, 200, 100, 'Option 2')
 option3 = Button((0, 255, 0), 520, 500, 200, 100, 'Option 3')
@@ -40,14 +43,15 @@ options.add(option1, option2, option3)
 make_button(screen, options, "yurim/airplanebackground.png")
 
 missiles = pygame.sprite.Group()  # 미사일들을 관리하는 Group 객체 missiles 생성
-missiles.add(DrunkMissile(100, 100, 0), MiniMissile(100, 400, 0), DrunkMissile(100, 700, 0))
-# 미사일 객체를 100, 100 좌표에 보는 각도 0도로 생성해서 missiles 그룹에 추가
-
 user_plane = Airplane(400, 400)  # 사용자 비행기 객체 생성
 
 clock = pygame.time.Clock()  # clock (화면 리프레시 속도 조절용)
+start_time = pygame.time.get_ticks()    # 게임 시작 시간 저장
+
 running = True
 while running:
+    time_since_enter = (pygame.time.get_ticks() - start_time)//1000  # 게임 시작 이후 진행 시간을 점수로 표시(초 단위)
+    level = time_since_enter//10 + 1    # 레벨은 10초당 1레벨 증가로 지정
     events = pygame.event.get()  # 이벤트 모음
     for event in events:
         if event.type == pygame.QUIT:  # 닫으면 나가기
@@ -55,8 +59,17 @@ while running:
     screen.fill((102, 204, 255))    # 배경 사이 틈 같은색으로 매꾸기
 
     backgrounds.update(screen, user_plane.vel)  # backgrounds Group 내의 모든 background 에 대해 update() 함수 실행
+
+    add_missile(missiles, level)   # 게임 레벨에 따른 미사일 생성
     missiles.update(screen, user_plane.loc, user_plane.vel)  # missiles 에 대해 실행
     user_plane.update(screen, events)  # user_plane 의 업데이트 실행
+
+    # 화면에 게임 레벨, 점수 표시
+    font = pygame.font.Font("Teko-Regular.ttf", 40)
+    level_text = font.render('Level ' + str(level), 1, (0, 0, 0))
+    screen.blit(level_text, (10, 5))
+    score_text = font.render('Score : ' + str(time_since_enter), 1, (0, 0, 0))
+    screen.blit(score_text, (10, 40))
 
     plane_missiles_collisions = pygame.sprite.spritecollide(user_plane, missiles, True, collided=pygame.sprite.collide_mask)
 
