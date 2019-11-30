a93a5feb82ac5ed0ce7ad361d698c86135afdee6
yurim <leeyou6757@sasa.hs.kr>
2019-11-30 20:00:22 +0900

    아이템 생성 및 보너스 포인트에 따른 게임 지속 기능 수정 (아이템 먹는 기능 오류)

--- main_practice.py
+++ main_practice.py
@@ -17,12 +17,10 @@ from yurim.button import *
 from yurim.addmissile import *
 from yurim.items import *
 
-
 pygame.init()
 
 screen = pygame.display.set_mode((800, 800))
 pygame.display.set_caption("Missiles!")
-
 
 backgrounds = pygame.sprite.Group()
 bg_length = 800
@@ -74,7 +72,7 @@ clock = pygame.time.Clock()  # clock (화면 리프레시 속도 조절용)
 start_time = pygame.time.get_ticks()    # 게임 시작 시간 저장
 
 running = True
-bonus = 0
+bonus = 1
 while running:
     time_since_enter = (pygame.time.get_ticks() - start_time)//1000 + bonus  # 게임 시작 이후 진행 시간을 점수로 표시(초 단위)
     level = time_since_enter//10 + 1    # 레벨은 10초당 1레벨 증가로 지정
